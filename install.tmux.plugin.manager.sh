56b154d2bf42de27fb45d179334625e926b16f33
Alex Coulton <alexcoulton@gmail.com>
2025-07-19 11:59:41 +0100

    update

--- install.tmux.plugin.manager.sh
+++ install.tmux.plugin.manager.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/alexcoulton/vim ~/vimrc
 
 cp ~/vimrc/.vimrc ~/.vimrc
 cp ~/vimrc/.tmux.conf ~/.tmux.conf
